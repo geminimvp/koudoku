3a67c07b416c4495cf8b3850838c78dab3b52129
jacobherrington <jacobherringtondeveloper@gmail.com>
2019-02-20 17:01:17 -0600

    Remove card attribute

--- app/concerns/koudoku/subscription.rb
+++ app/concerns/koudoku/subscription.rb
@@ -71,7 +71,7 @@ module Koudoku::Subscription
               customer_attributes = {
                 description: subscription_owner_description,
                 email: subscription_owner_email,
-                card: credit_card_token, # obtained with Stripe.js
+                plan: plan.stripe_id,
                 metadata: subscription_owner_metadata
               }
 
